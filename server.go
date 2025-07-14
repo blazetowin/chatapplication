a009a30644af348e5cac5565f2b220f8433c5bca
blazetowin <ardaolgun28@gmail.com>
2025-07-14 20:05:12 +0300

    Aktif kullanıcı listesi ve emojiler

--- server.go
+++ server.go
@@ -1,104 +1,126 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "sync"
-    "github.com/gorilla/websocket" 
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"sync"
+
+	"github.com/gorilla/websocket"
 )
 
 // WebSocket bağlantısını HTTP üzerinden yükseltmek için kullanılır
 var upgrader = websocket.Upgrader{
-    CheckOrigin: func(r *http.Request) bool {
-        return true
-    },
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
-// Bağlantı: kullanıcı eşlemesi için bir harita (her istemcinin kullanıcı adını saklarız)
+// Tüm istemcileri ve kullanıcı adlarını eşle
 var clients = make(map[*websocket.Conn]string)
-
-// Erişimleri güvenli hale getirmek için mutex
 var clientsMutex = sync.Mutex{}
 
-// Mesajları göndermek için kanal
-var broadcast = make(chan string) // Artık []byte değil, doğrudan string mesaj gönderiyoruz
+// Gelen mesajlar burada toplanır
+var broadcast = make(chan string)
 
 func main() {
-    http.HandleFunc("/ws", handleConnections)
-    http.Handle("/", http.FileServer(http.Dir("./")))
+	http.HandleFunc("/ws", handleConnections)
+	http.Handle("/", http.FileServer(http.Dir("./")))
 
-    // Mesajları dağıtan fonksiyonu başlat
-    go handleMessages()
+	go handleMessages()
 
-    fmt.Println("WebSocket sunucusu http://localhost:8080/ws adresinde çalışıyor...")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("WebSocket sunucusu http://localhost:8080/ws adresinde çalışıyor...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-// Bu fonksiyon her yeni bağlantı için çalışır
 func handleConnections(w http.ResponseWriter, r *http.Request) {
-    ws, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        log.Println("Yükseltme hatası:", err)
-        return
-    }
-    defer ws.Close()
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Yükseltme hatası:", err)
+		return
+	}
+	defer ws.Close()
 
-    var username string
+	var username string
 
-    // İlk gelen mesajı kullanıcı adı olarak al
-    _, firstMsg, err := ws.ReadMessage()
-    if err != nil {
-        log.Println("İlk mesaj (kullanıcı adı) okunamadı:", err)
-        return
-    }
-    username = string(firstMsg)
+	// İlk gelen mesaj kullanıcı adıdır
+	_, msg, err := ws.ReadMessage()
+	if err != nil {
+		log.Println("Kullanıcı adı alınamadı:", err)
+		return
+	}
+	username = string(msg)
 
-    // Kullanıcıyı clients map’ine ekle
-    clientsMutex.Lock()
-    clients[ws] = username
-    clientsMutex.Unlock()
+	clientsMutex.Lock()
+	clients[ws] = username
+	clientsMutex.Unlock()
 
-    // Herkese bu kişinin katıldığını duyur
-    joinMsg := fmt.Sprintf("🔵 %s katıldı", username)
-    broadcast <- joinMsg
+	// Katılım mesajı
+	broadcast <- fmt.Sprintf("🔵 %s katıldı", username)
 
-    for {
-        // Yeni mesajları oku
-        _, msg, err := ws.ReadMessage()
-        if err != nil {
-            // Eğer bağlantı koparsa ayrılma mesajı gönder
-            log.Printf("%s bağlantı koptu: %v\n", username, err)
+	// Aktif kullanıcı listesini gönder
+	sendActiveUsers()
 
-            clientsMutex.Lock()
-            delete(clients, ws)
-            clientsMutex.Unlock()
+	for {
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			log.Printf("Kullanıcı ayrıldı: %s\n", username)
 
-            leaveMsg := fmt.Sprintf("🔴 %s ayrıldı", username)
-            broadcast <- leaveMsg
-            break
-        }
+			clientsMutex.Lock()
+			delete(clients, ws)
+			clientsMutex.Unlock()
 
-        // Kullanıcının mesajını al ve ismiyle birlikte yay
-        fullMsg := fmt.Sprintf("%s: %s", username, msg)
-        broadcast <- fullMsg
-    }
+			broadcast <- fmt.Sprintf("🔴 %s ayrıldı", username)
+			sendActiveUsers()
+			break
+		}
+
+		text := emojiParser(string(msg))
+		broadcast <- fmt.Sprintf("%s: %s", username, text)
+	}
 }
 
-// Gelen her mesajı tüm istemcilere gönderir
 func handleMessages() {
-    for {
-        msg := <-broadcast
+	for {
+		msg := <-broadcast
+		clientsMutex.Lock()
+		for client := range clients {
+			err := client.WriteMessage(websocket.TextMessage, []byte(msg))
+			if err != nil {
+				log.Println("Mesaj gönderme hatası:", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+		clientsMutex.Unlock()
+	}
+}
 
-        clientsMutex.Lock()
-        for client := range clients {
-            err := client.WriteMessage(websocket.TextMessage, []byte(msg))
-            if err != nil {
-                log.Println("Mesaj gönderilemedi:", err)
-                client.Close()
-                delete(clients, client)
-            }
-        }
-        clientsMutex.Unlock()
-    }
+// Aktif kullanıcı listesini tüm istemcilere gönder
+func sendActiveUsers() {
+	userList := "👥 Aktif kullanıcılar: "
+	clientsMutex.Lock()
+	users := []string{}
+	for _, name := range clients {
+		users = append(users, name)
+	}
+	clientsMutex.Unlock()
+	userList += strings.Join(users, ", ")
+	broadcast <- userList
+}
+
+// Basit emoji dönüştürücü
+func emojiParser(text string) string {
+	replacements := map[string]string{
+		":smile:": "😄",
+		":heart:": "❤️",
+		":fire:":  "🔥",
+		":thumbs:": "👍",
+		":ok:":    "👌",
+	}
+	for key, val := range replacements {
+		text = strings.ReplaceAll(text, key, val)
+	}
+	return text
 }
